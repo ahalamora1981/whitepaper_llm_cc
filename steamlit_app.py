cde48ee01565cb60740116840fea7399417d61b3
ahalamora1981 <96462464+ahalamora1981@users.noreply.github.com>
2023-04-02 15:54:17 +0800

    Create steamlit_app.py

---
+++ steamlit_app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import base64
+
+# 设置页面标题
+st.title('大语言模型在客服中心的应用前景')
+
+# 加载PDF文件
+def show_pdf(file_path):
+    with open(file_path, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+show_pdf("D:\Dev\OpenAI\llm_for_cc\llm_for_cc.pdf")
