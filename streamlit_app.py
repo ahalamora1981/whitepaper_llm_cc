362aa3c3eea608a2fc1cf3c7e50ce73a669ccaeb
ahalamora1981 <96462464+ahalamora1981@users.noreply.github.com>
2023-04-02 16:34:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,10 +10,10 @@ st.write(os.getcwd())
 file_path = "llm_for_cc.pdf"
 
 # 加载PDF文件
-with open(file_path, "rb") as f:
-    base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
+f = open(file_path, "rb")
+base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+st.markdown(pdf_display, unsafe_allow_html=True)
 
 # st.markdown("""
 # <embed src="/app/whitepaper_llm_cc/llm_for_cc.pdf" width="1000" height="800">
