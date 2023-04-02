355e54be5aac2418c96f4f6c8b2e38236e63b099
ahalamora1981 <96462464+ahalamora1981@users.noreply.github.com>
2023-04-02 16:05:13 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,11 +7,15 @@ import os
 st.title('大语言模型在客服中心的应用前景')
 st.write(os.getcwd())
 
-# 加载PDF文件
-def show_pdf(file_path):
-    with open(file_path, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1200" height="800" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
+# # 加载PDF文件
+# def show_pdf(file_path):
+#     with open(file_path, "rb") as f:
+#         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+#     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1200" height="800" type="application/pdf"></iframe>'
+#     st.markdown(pdf_display, unsafe_allow_html=True)
 
-show_pdf("/app/whitepaper_llm_cc/llm_for_cc.pdf")
+# show_pdf("/app/whitepaper_llm_cc/llm_for_cc.pdf")
+
+st.markdown("""
+<embed src="/app/whitepaper_llm_cc/llm_for_cc.pdf" width="1000" height="800">
+""", unsafe_allow_html=True)
